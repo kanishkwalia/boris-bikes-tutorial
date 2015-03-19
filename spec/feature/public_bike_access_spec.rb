ca11dbd9ab87bf0af46330cb7de1ed325460780d
Kanishk Walia <kanishk.walia@gmail.com>
2015-03-19 17:53:27 +0000

    Added public_bike_access_spec.rb

---
+++ spec/feature/public_bike_access_spec.rb
@@ -0,0 +1,24 @@
+require 'docking_station'
+require 'capybara/rspec'
+
+feature 'member of public accesses bike' do
+	let(:docking_station) { DockingStation.new }
+  scenario 'docking station releases a bike that is not broken' do
+  	bike = Bike.new 
+  	docking_station.dock bike
+  	bike = docking_station.release_bike
+  	expect(bike).not_to be_broken
+  end
+
+  scenario 'docking station unable to release as none available' do
+  	expect { docking_station.release_bike }.to raise_error 'No bikes available'
+  end
+
+  scenario 'docking station will not make broken bikes available' do
+  	broken_bike = Bike.new
+  	broken_bike.break
+  	docking_station.dock broken_bike
+  	expect { docking_station.release_bike }.to raise_error 'No bikes available'
+  end
+
+end
