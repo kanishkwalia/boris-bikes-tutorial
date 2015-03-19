ca2eacd188a59233cfa7f09badf925adebc65b8a
Kanishk Walia <kanishk.walia@gmail.com>
2015-03-19 17:51:26 +0000

    Added bike.rb

---
+++ lib/bike.rb
@@ -0,0 +1,18 @@
+class Bike
+
+	attr_reader :broken
+	alias_method :broken?, :broken
+
+	def initialize
+		@broken = false
+	end
+  
+  def broken?
+  	@broken
+  end
+
+  def break
+  	@broken = true
+  end
+
+end
