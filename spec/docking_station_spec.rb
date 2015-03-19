791b89ab2196b2359d3b654eac0303ddcef9f6c3
Kanishk Walia <kanishk.walia@gmail.com>
2015-03-19 17:52:48 +0000

    Added docking_station_spec.rb

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,21 @@
+require 'docking_station'
+
+describe DockingStation do
+	it 'releases bikes that are not broken' do
+		subject.dock double :bike, broken?: false
+		bike = subject.release_bike
+		expect(bike).not_to be_broken
+	end
+	it 'can dock a bike' do
+		expect(subject.dock :bike).to be nil
+	end
+	it 'raises an error when full' do
+		20.times { subject.dock double :bike }
+		expect { subject.dock double :bike }.to raise_error 'Station Full'
+	end
+	it 'does not release broken bikes' do
+		broken_bike = double :bike, broken?: true
+		subject.dock broken_bike
+		expect { subject.release_bike }.to raise_error 'No bikes available'
+	end
+end
