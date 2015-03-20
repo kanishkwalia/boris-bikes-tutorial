a5dc469ed5adab7a824ced7e619aa11dec706dd1
Kanishk Walia <kanishk.walia@gmail.com>
2015-03-20 16:30:58 +0000

    Added spec/van_spec.rb

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,8 +4,19 @@ describe Van do
 	let(:van){Van.new}
 	let(:bike){double :bike}
 	
-	it 'can load a bike' do
-		van.load bike
+	it 'can accept a bike' do
+		van.accept bike
 		expect(van.bikes).to eq [bike]
+	end
+
+	it 'can release a bike' do
+		van.accept bike
+		van.release bike
+		expect(van.bikes).to eq []
+	end
+
+	it 'raises an error when full' do
+		10.times { subject.accept double :bike }
+		expect { subject.accept double :bike }.to raise_error "Van full"
 	end
 end
